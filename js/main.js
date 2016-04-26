895575173bc6670d85e943966e808314f237830b
AIRTucha <airtucha@yandex.ru>
2016-04-26 18:24:23 +0300

    about is added

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ require('jquery-ui');
 
 (function(namespace) {window.onload = function(){
   setLayout();
-
+  createAbout();
   var s = new SortVis(20, 
                       function(a, b) {return a < b;},
                       $('#main').width() * 0.99,
@@ -77,6 +77,26 @@ require('jquery-ui');
     s.setAlgo($(this).find('option:selected').val())
     updataSlider();
   });
+  
+  
+  
+  $('.savonia').click(function(){$(".about").fadeIn(500)});
+  
+    function createAbout(){
+    $('body').append('<div class = "about"><div class = "about_content"></div><div class = "about_exit"></div></div>');
+    
+    $('.about_content').append('<h1>About</h1><br/>' + 
+                       '<p>The visualisation is created by <a target = "_blank" href = "https://github.com/AIRTucha">Alexey Tukalo</a> for Advanced Algorithms and Data Structures course at <a target = "_blank" href = "http://portal.savonia.fi/amk/">Savonia University of Applied Sciences</a>.' +
+                       '<p>The visualisation represents sevent the most common sorting algorithms and gives user an opportunity to brows thought them in a different way.</p>' +
+                      '<p>You are welcome to fork and improve the framework on <a target = "_blank" href = "https://github.com/AIRTucha/SortVis">GitHub</a>.</p>');
+      
+      $('a').css({'color' : '#AA0077'});
+    
+      $(".about_exit").click(function(){ $(".about").fadeOut(500)});
+      $(".about").hide();
+    
+  }
+ 
   
  function updataSlider(){
     $('#slyder').slider( "option", "value", s.getStep()).slider( "option", "max", s.getLogSize());
